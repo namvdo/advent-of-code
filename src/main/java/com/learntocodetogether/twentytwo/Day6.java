932008d25f8b873a52cb050f9db7be64ed131c0b
Nam Van Do <nam.dv@coccoc.com>
2023-01-01 09:52:54 +0700

    Day 6 - part 1

---
+++ src/main/java/com/learntocodetogether/twentytwo/Day6.java
@@ -0,0 +1,34 @@
+package com.learntocodetogether.twentytwo;
+
+import com.learntocodetogether.utils.Utils;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * @author namvdo
+ */
+@Slf4j
+public class Day6 {
+    public static int solvePart1(String input) {
+        char[] chars = input.toCharArray();
+        for(int i = 0; i < chars.length - 4; i++) {
+            Set<Character> set = new HashSet<>();
+            for(int j = i; j < i + 4; j++) {
+               set.add(chars[j]);
+               if (set.size() == 4) {
+                   return j + 1;
+               }
+            }
+            set.clear();
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        log.info("{}", solvePart1(Utils.getResourceAsString("2022/day6")));
+    }
+}
