4cfb3c3f003714575a1713a83df0640dbabea1c8
Nam Van Do <nam.dv@coccoc.com>
2023-08-29 23:01:25 +0700

    Add Day 10 part 2

--- 2022/Day10.java
+++ 2022/Day10.java
@@ -3,6 +3,7 @@ package com.learntocodetogether.twentytwo;
 import com.learntocodetogether.utils.Utils;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -21,6 +22,42 @@ public class Day10 {
 	public static void main(String[] args) {
 		String input = Utils.get("./input/day10");
 		System.out.println("Part 1: " + solve(input));
+		System.out.println("Part 2:\n" + solvePart2(input));
+	}
+
+	static final int COLS = 40;
+	static final int ROWS = 6;
+	static final int SPRITE_WIDTH = 3;
+	static char getPixel(int cycle, int x) {
+		int curCol = (cycle - 1) % COLS;
+		if (Math.abs(curCol - x) <= SPRITE_WIDTH / 2) {
+			return '█';
+		} else {
+			return ' ';
+		}
+	}
+	public static String solvePart2(String input) {
+		char[] grid = new char[ROWS * COLS];
+		int x = 1;
+		int cycle = 1;
+		List<Instruction> instructions = parse(input);
+		for(Instruction in : instructions) {
+			grid[cycle - 1]= getPixel(cycle, x);
+			cycle += 1;
+			if (in.opt() == Opt.ADD_X) {
+				grid[cycle - 1] = getPixel(cycle, x);
+				cycle += 1;
+				x += in.val();
+			}
+		}
+		StringBuilder sb = new StringBuilder();
+		for(int i = 1, j = 0; i <= COLS * ROWS; i++, j++) {
+			sb.append(grid[j]);
+			if (i % COLS == 0) {
+				sb.append("\n");
+			}
+		}
+		return sb.toString();
 	}
 
 	public static int solve(String input) {
